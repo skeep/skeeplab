dca723ba28ecc127e2d7e07283407fd02c432528
Suman Paul <sumank.paul@gmail.com>
2013-01-09 14:13:24 +0530

    range update

--- range/script.js
+++ range/script.js
@@ -4,5 +4,5 @@
 	slider.onchange = function(){
 		var pos = parseInt(slider.value, 10);
 		slider.style.background = '-webkit-linear-gradient(left, #009eed 0%,#009eed '+pos+'%,#002849 '+(pos+1)+'%,#002849 100%)';
-	}
+	};
 })();
