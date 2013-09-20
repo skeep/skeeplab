c18a9624e43fe86cd768e0495f744889551d62be
Suman Paul <sumank.paul@gmail.com>
2013-09-20 14:34:27 +0530

    remove everything

--- README.md
+++
@@ -1,4 +0,0 @@
-skeeplab
-========
-
-My Experiments
--- range/script.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	var slider;
-	slider = document.getElementById('range');
-	slider.onchange = function(){
-		var pos = parseInt(slider.value, 10);
-		slider.style.background = '-webkit-linear-gradient(left, #009eed 0%,#009eed '+pos+'%,#002849 '+(pos+1)+'%,#002849 100%)';
-	};
-})();
